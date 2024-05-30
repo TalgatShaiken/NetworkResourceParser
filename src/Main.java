97c5df76aeff9a75007f1b1b1941755d117258cf
Talgat Shaiken <talgat.shaiken@yandex.kz>
2024-05-31 01:29:18 +0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Проект по сетевому парсеру на java. Начало");
+        System.out.println("Серега, думаю ты напишешь запросы для парсинга html страниц для начала?");
     }
 }
